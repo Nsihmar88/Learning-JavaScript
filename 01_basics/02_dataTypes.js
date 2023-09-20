92ec727b489b09ebbf7494fc49b3d9a33dc11307
Naresh Singh Sihmar <111731321+Nsihmar88@users.noreply.github.com>
2023-09-20 10:15:33 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3 + 5) // We are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high // don't use this style.
+
+
+let name = "Naresh Singh Sihmar"
+let age = 35
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (type of null is Object)
+// undifined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // Object
